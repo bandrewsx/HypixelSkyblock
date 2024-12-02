4275086f82c28d7630749a86e2f3a6a68929f480
Brianna Andrews <bwoandrews@gmail.com>
2024-12-02 15:48:05 -0700

    Create ModConfig.java

---
+++ src/main/java/com/bandrewsx/skyblockmod/ModConfig.java
@@ -0,0 +1,10 @@
+package com.bandrewsx.skyblockmod.config;
+
+public class ModConfig {
+    public static boolean enableFairySoulLocator = true;
+    
+    public static void loadConfig() {
+        // Load config from file or defaults.
+        System.out.println("Configuration loaded.");
+    }
+}
