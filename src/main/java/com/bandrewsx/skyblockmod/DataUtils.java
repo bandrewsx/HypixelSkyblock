e3d86f986372b489f6d369a91be918f160b47d94
Brianna Andrews <bwoandrews@gmail.com>
2024-12-02 15:46:00 -0700

    Create DataUtils.java

---
+++ src/main/java/com/bandrewsx/skyblockmod/DataUtils.java
@@ -0,0 +1,8 @@
+package com.bandrewsx.skyblockmod.utils;
+
+public class DataUtils {
+
+    public static int calculateMana(int baseMana, int modifiers) {
+        return baseMana + modifiers;
+    }
+}
